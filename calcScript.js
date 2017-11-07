37a25ae36bcffe6b7c7ef51b1dd3d923633dd74e
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-07 15:15:34 +0000

    First row of buttons added

--- calcScript.js
+++ calcScript.js
@@ -1,4 +1,10 @@
 
+//108 * 4 == keypad width
+
+// button width == 100px
+// amount inbetween == 8px
+
+
 //Number buttons 10
 
 //arithmetic buttons 4
@@ -15,6 +21,13 @@
   let calcPrevDisp;
   let calcMainDisp;
 
+
+
+  //Button Assigns
+  let clearButton;
+  let divideButton;
+  let multiplyButton;
+
   function buildCalc(){
     console.log('building');
     const calcTop = document.createElement('div');
@@ -30,12 +43,34 @@
     calcMainDispl.id = "calcMainDisp";
     calcMainDispl.value = "0";
     calcTop.appendChild(calcMainDispl);
+
+    const keypad = document.createElement('div');
+    keypad.id = "calcPad";
+    app.appendChild(keypad);
+
+    const clearB = document.createElement('button');
+    clearB.id = 'clearButton';
+    clearB.innerHTML = "C";
+    keypad.appendChild(clearB);
+
+    const divideB = document.createElement('button');
+    divideB.id = 'divideButton';
+    divideB.innerHTML = "&times";
+    keypad.appendChild(divideB);
+
+    const multiplyB = document.createElement('button');
+    multiplyB.id = "multiplyButton";
+    multiplyB.innerHTML = "&#247";
+    keypad.appendChild(multiplyB);
   }
 
   function idAssign(){
     calcHead = document.getElementById('calcHead');
     calcPrevDisp = document.getElementById('calcPrevDisp');
     calcMainDisp = document.getElementById('calcMainDisp');
+    clearButton = document.getElementById('clearButton');
+    divideButton = document.getElementById('divideButton');
+    multiplyButton = document.getElementById('multiplyButton');
   }
 
  (function init(){
